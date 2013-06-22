d0377ef7c9fcf6cc30ac70fc711c30ca5e314a7e
Max Lunin <nut.code.monkey@gmail.com>
2013-06-22 13:28:32 +0300

    Initial Commit

---
+++ ssgenerator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ssgenerator
+//
+//  Created by Max Lunin on 22.06.13.
+//  Copyright (c) 2013 Max Lunin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
